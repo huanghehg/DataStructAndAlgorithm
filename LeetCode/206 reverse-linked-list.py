b87417ca6ba9e25a5a461de6fd28a52fd40aa8af
huanghe <huanghe@tuya.com>
2020-06-23 10:25:55 +0800

    update reverse-linked-list

---
+++ LeetCode/206 reverse-linked-list.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __str__(self):
+        head = self
+        des = ''
+        while head is not None:
+            des = des + head.val + ' -> '
+            head = head.next
+        des += "NULL"
+        return des
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # 迭代
+        pre, cur = None, head
+        while cur is not None:
+            next = cur.next
+            cur.next = pre
+            pre = cur
+            cur = next
+        return pre
+
+        # # 递归
+        # if head is None or head.next is None:
+        #     return head
+        # rHead = self.reverseList(head.next)
+        # head.next.next = head
+        # head.next = None
+        # return rHead
+
+        # 栈
+        # stack = []
+        # while head is not None:
+        #     stack.append(head)
+        #     head = head.next
+        # stack[0].next = None
+        # i = 1
+        # while i < len(stack) :
+        #     stack[i].next = stack[i - 1]
+        #     i += 1
+        # return stack[-1]
+
+
+if __name__ == '__main__':
+    node1 = ListNode("1")
+    node2 = ListNode("2")
+    node3 = ListNode("3")
+    node4 = ListNode("4")
+    node5 = ListNode("5")
+    node6 = ListNode("6")
+    node7 = ListNode("7")
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+    node5.next = node6
+    node6.next = node7
+    print(node1)
+    print(Solution().reverseList(node1))
