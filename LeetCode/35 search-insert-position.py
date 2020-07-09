4602899d971280670bc81600b605c35c635af3fe
huanghe <huanghe@tuya.com>
2020-07-09 21:52:06 +0800

    feat: leetcode 35

---
+++ LeetCode/35 search-insert-position.py
@@ -0,0 +1,41 @@
+# 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+#
+# 你可以假设数组中无重复元素。
+#
+# 示例 1:
+#
+# 输入: [1,3,5,6], 5
+# 输出: 2
+# 示例 2:
+#
+# 输入: [1,3,5,6], 2
+# 输出: 1
+# 示例 3:
+#
+# 输入: [1,3,5,6], 7
+# 输出: 4
+# 示例 4:
+#
+# 输入: [1,3,5,6], 0
+# 输出: 0
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/search-insert-position
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution:
+    def searchInsert(self, nums: list, target: int) -> int:
+        if target in nums: return nums[target]
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] < target:
+                start = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+        return start
+
+
+if __name__ == '__main__':
+    print(Solution().searchInsert([1, 3, 5, 7, 9], 10))
