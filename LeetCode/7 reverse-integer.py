df1e866fe281932d9c8e58a2ae804cce9f7308cd
huanghe <huanghe@tuya.com>
2020-07-14 09:55:07 +0800

    feat: leetcode 7

---
+++ LeetCode/7 reverse-integer.py
@@ -0,0 +1,48 @@
+# 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+#
+# 示例 1:
+#
+# 输入: 123
+# 输出: 321
+#  示例 2:
+#
+# 输入: -123
+# 输出: -321
+# 示例 3:
+#
+# 输入: 120
+# 输出: 21
+# 注意:
+#
+# 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/reverse-integer
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if -10 < x < 10: return x
+        rev = 0
+        MAXNUM = pow(2, 31) - 1
+        MINNUM = -pow(2, 31)
+
+        a = 1
+        if x < 0:
+            a = -1
+            if x > MINNUM:
+                x = abs(x)
+            else:
+                return 0
+        while x != 0:
+            r = x % 10
+            if MAXNUM // 10 < rev or MAXNUM - rev * 10 < r:
+                return 0
+            rev = rev * 10 + r
+            x = x // 10
+        return rev * a
+
+
+if __name__ == '__main__':
+    print(Solution().reverse(12345))
