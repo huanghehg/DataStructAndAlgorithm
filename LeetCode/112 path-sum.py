0c6145c5bb5d4ed6dfaab2596e55a6c80175caef
huanghe <huanghe@tuya.com>
2020-08-05 09:58:39 +0800

    feat: leetCode 112

---
+++ LeetCode/112 path-sum.py
@@ -0,0 +1,56 @@
+# 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+#
+# 说明: 叶子节点是指没有子节点的节点。
+#
+# 示例: 
+# 给定如下二叉树，以及目标和 sum = 22，
+#
+#               5
+#              / \
+#             4   8
+#            /   / \
+#           11  13  4
+#          /  \      \
+#         7    2      1
+# 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+#
+# 通过次数122,166提交次数239,979
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/path-sum
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+import collections
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        # 递归法
+        # if root is None: return False
+        # if root.left is None and root.right is None: return root.val == sum
+        # return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+
+        # 广度优先
+        if not root:
+            return False
+        que_node = collections.deque([root])
+        que_val = collections.deque([root.val])
+        while que_node:
+            now = que_node.popleft()
+            temp = que_val.popleft()
+            if not now.left and not now.right:
+                if temp == sum:
+                    return True
+                continue
+            if now.left:
+                que_node.append(now.left)
+                que_val.append(now.left.val + temp)
+            if now.right:
+                que_node.append(now.right)
+                que_val.append(now.right.val + temp)
+        return False
+
+
